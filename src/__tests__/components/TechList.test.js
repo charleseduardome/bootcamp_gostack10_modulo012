eac12a08431717f9d057384ae936ad997ab00684
charleseduardome <charleseduardo.me@gmail.com>
2020-03-27 19:19:29 -0300

    Mock do useDispatch

--- src/__tests__/components/TechList.test.js
+++ src/__tests__/components/TechList.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import 'jest-localstorage-mock';
 
 import TechList from '~/components/TechList';
+
+import { addTech } from '~/store/modules/techs/actions';
 
 jest.mock('react-redux');
 
@@ -19,5 +21,18 @@ describe('TechList component', () => {
 
     expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
     expect(getByTestId('tech-list')).toContainElement(getByText('ReactJS'));
+  });
+
+  it('should be able to add new tech', () => {
+    const { getByTestId, getByLabelText } = render(<TechList />);
+
+    const dispatch = jest.fn();
+
+    useDispatch.mockReturnValue(dispatch);
+
+    fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' } });
+    fireEvent.submit(getByTestId('tech-form'));
+
+    expect(dispatch).toHaveBeenCalledWith(addTech('Node.js'));
   });
 });
